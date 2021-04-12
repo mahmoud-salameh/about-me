938022ebd5cbba331de3580d620685cad336f685
mahmoud-salameh <mahmoud4561@hotmail.com>
2021-04-12 22:37:29 +0300

    the new file about-me and the making of js and css

---
+++ about-me/JS/app.js
@@ -0,0 +1,65 @@
+'use strict';
+
+let visitorName = prompt ('what is your name').toLowerCase();
+//console.log(visitorName);
+
+  if(visitorName === 'mahmoud') {
+   alert('Greeting ' + visitorName);
+   } else {
+   alert('Please check you answers');
+   }
+
+
+let start = prompt ('can we play a game to test how much you know about me').toLowerCase();
+//console.log(start);
+  if(start === 'yes' || start === 'y') {
+      alert('thank you let us start')
+  } else if
+      (start === 'no' || start === 'n')
+  {
+      alert('it will be quick do not worry');
+    }
+
+let universty = ('MEU').toUpperCase();
+//console.log(universty);
+
+let first = prompt('do you know which universty i studied it').toLowerCase();
+//console.log(first);
+  if(first === 'yes' || first === 'y') {
+      alert('that is great');}
+      else if(first === 'no' || first === 'n') {
+          alert('it is ok the currect answer is '  + universty);
+      }
+
+let team = prompt('do you know which football club i support').toLowerCase();
+//console.log(team);
+  if(team === 'yes' || team === 'y') {
+    alert('yes it is barcelona the best team in the world');}
+  else if(team === 'no' || team === 'n') {
+          alert('it is ok the currect answer is barcelona the best team in the world');
+      }
+
+let degree = prompt('do you know what is my first major').toLowerCase();
+//console.log(degree);
+    if(degree === 'yes' || degree === 'y') {
+        alert('civil engineering you are right');}
+    else if(degree === 'no' || degree === 'n') {
+                alert('it all right you can read more about my here in this page');
+            }
+
+            
+let learn = prompt('do you know what I study now').toLowerCase();
+//console.log(learn);
+    if(learn === 'yes' || learn === 'y') {
+        alert('software development yeah you are correct');}
+    else if(learn === 'no' || learn === 'n') {
+                alert('I study now software development and it look like a great thing');
+            }
+
+
+
+
+
+      
+
+
